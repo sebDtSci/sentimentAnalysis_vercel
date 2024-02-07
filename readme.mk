92babadc20483c2b37fb15e19e11c1625d61ab15
Seb <121176289+sebDtSci@users.noreply.github.com>
2024-02-07 18:03:05 +0100

    add readme

---
+++ readme.mk
@@ -0,0 +1,24 @@
+## Sentiment Analysis
+
+### Structure :
+```bash
+├── app.py
+├── model
+│   ├── __pycache__
+│   │   ├── __init__.cpython-311.pyc
+│   │   ├── main.cpython-310.pyc
+│   │   └── main.cpython-311.pyc
+│   └── main.py
+├── readme.mk
+├── requirements.txt
+├── static
+│   └── style.css
+├── templates
+│   └── index.html
+├── test
+│   ├── __pycache__
+│   │   ├── test_model.cpython-310-pytest-7.4.4.pyc
+│   │   └── test_model.cpython-311-pytest-8.0.0.pyc
+│   └── test_model.py
+└── vercel.json
+```
